8e3d602d564a383fc150213b751b75ef3a96db92
Lucas de Jesus <lucas_moreira110@hotmail.com>
2021-04-02 23:51:15 -0300

    chore: Implements Aluguel Class

--- LocadoraDeCarro/Presentacao/Dados/Models/AluguelDataModel.cs
+++ LocadoraDeCarro/Presentacao/Dados/Models/AluguelDataModel.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace Dados.Models
 {
-    class AluguelDataModel
+   public  class AluguelDataModel
     {
+        public int Id { get; set; }
+
+        public int  ClienteId { get; set; }
+
+        public int CarroId { get; set; }
+
+        public DateTime Datatransacao { get; set; }
+
+        public DateTime DataReserva { get; set; }
+
+        public Boolean Efetivado { get; set; }
     }
 }
