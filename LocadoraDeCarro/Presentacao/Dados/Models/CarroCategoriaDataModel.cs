2c9131d0b50dde5998afdb993460872e2100bd5b
Lucas de Jesus <lucas_moreira110@hotmail.com>
2021-04-02 23:23:13 -0300

    fix: Creating CarCategory class

---
+++ LocadoraDeCarro/Presentacao/Dados/Models/CarroCategoriaDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dados.Models
+{
+    public class CarroCategoriaDataModel
+    {
+        public int Id { get; set; }
+
+        public string Descricao  { get; set; }
+
+
+    }
+}
