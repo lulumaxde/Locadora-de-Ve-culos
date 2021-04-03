19fa4740f8d62e28b85fe4602bbf5123af1a853e
Lucas de Jesus <lucas_moreira110@hotmail.com>
2021-04-02 22:09:11 -0300

    chore: add and config middlewares

--- LocadoraDeCarro/Presentacao/LocadoraDeCarro/Startup.cs
+++ LocadoraDeCarro/Presentacao/LocadoraDeCarro/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LocadoraDeCarro.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -41,10 +42,11 @@ namespace LocadoraDeCarro
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
+
+            //custom Middlewares
+            app.UseMiddleware<FriendListMiddleware>(Configuration["AllowedHosts"]);
 
             app.UseEndpoints(endpoints =>
             {
