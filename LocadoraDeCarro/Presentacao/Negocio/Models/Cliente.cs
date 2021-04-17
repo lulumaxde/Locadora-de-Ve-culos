09b5c46abe9759f8c4d6de151fc5644cbaebd5cb
Lucas de Jesus <lucas_moreira110@hotmail.com>
2021-04-17 19:25:27 -0300

    feat: Implementation of Email Validation

--- LocadoraDeCarro/Presentacao/Negocio/Models/Cliente.cs
+++ LocadoraDeCarro/Presentacao/Negocio/Models/Cliente.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Negocio.RepositorioDados;
+using Negocio.ServicoNegocio.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +8,8 @@ namespace Negocio.Models
 {
     public class Cliente
     {
+        private readonly IClienteServico _clienteServico;
+
         public int Id { get; private set; }
         public string Nome { get; private set; }
         public string Endereco { get; private set; }
@@ -13,6 +17,29 @@ namespace Negocio.Models
         public string Email { get; private set; }
         public string CarteiraDeMotorista { get; private set; }
         public string CartaoDeCredito { get; private set; }
+
+        public Cliente()
+        {
+
+        }
+
+            public Cliente(int id, string nome, string endereco, string telefone, string email, string carteirademotorista, string cartaodecredito, IClienteServico clienteServico)
+        {
+            Id = id;
+            Nome = nome;
+            Endereco = endereco;
+            Telefone = telefone;
+            Email = email;
+            CarteiraDeMotorista = carteirademotorista;
+            CartaoDeCredito = cartaodecredito;
+            
+            _clienteServico = clienteServico;
+        }
+
+        public bool EmailEstaDuplicado()
+        {
+            return _clienteServico.obterClientePorEmail(this.Email) != null;
+        }
 
     }
 }
